cb6711dfaabeb3a486f9fbcc52d39dc33cdc6d28
Samo <samo.zeleznik5@gmail.com>
2022-11-22 08:45:52 +0100

    Move alert and notice to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def display_notice
+    content_tag(:p, notice, class: "notice") if notice.present?
+  end
+
+  def display_alert
+    content_tag(:p, alert, class: "alert") if alert.present?
+  end
 end
