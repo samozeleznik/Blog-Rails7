0166601813004f4eadf0f3b135f2efc5e715d1db
Samo <samo.zeleznik5@gmail.com>
2022-12-08 09:34:27 +0100

    Add comments resource

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
   validates_presence_of :title, :body
+  has_many :comments, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments, only: [:update, :destroy, :create]
   devise_for :users
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
