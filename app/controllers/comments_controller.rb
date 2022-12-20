414dbe9f4ccbe03e901deff1d052d2e1dfe75488
Samo <samo.zeleznik5@gmail.com>
2022-12-20 09:10:46 +0100

    Add comment creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,23 @@
 class CommentsController < ApplicationController
+  def create
+    @post=Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to post_url(@post), notice: "Comment was successfully created." }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { redirect_to post_url(@post), alert: "There was a problem with your comment." }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Only allow a list of trusted parameters through.
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :comments, only: [:update, :destroy, :create]
   devise_for :users
-  resources :posts
+  
+  resources :posts do
+    resources :comments, only: [:update, :destroy, :create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
