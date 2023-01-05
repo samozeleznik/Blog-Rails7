62c0dac9aed65b4bb2d5aa7f419f6be34e0e379a
Samo <samo.zeleznik5@gmail.com>
2023-01-05 09:48:15 +0100

    add rich text

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,7 @@ class PostsController < ApplicationController
   # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
+    @post.published_at = DateTime.now if @post.published_at.blank?
     @post.user = current_user
 
     respond_to do |format|
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
   validates_presence_of :title, :body
   has_many :comments, dependent: :destroy
   scope :published, -> { where(published_at: nil).or(where("published_at <= ?", DateTime.now)) }
+  has_rich_text :body
 end
